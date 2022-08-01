9cf335b7ee38f9bfefad112644d890f61ad3bca7
Vlksju <xenya.vol2017@yandex.ru>
2022-08-01 17:01:06 +0500

    Solved task 21

---
+++ task21/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Программа котрая находит расстояние между двумя точками в 3D простанстве");
+Console.WriteLine("Введите координаты точек");
+
+Console.WriteLine("Координаты первой точки. X =");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y = ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Z = ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Координаты второй точки. X =");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Y = ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Z = ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow (X2-X1, 2) + Math.Pow (Y2-Y1, 2) + Math.Pow (Z2-Z1, 2));
+
+Console.WriteLine($"Расстояние = {distance}");
