963be87c193751677b8cb0df0593b7090295682f
Vlksju <xenya.vol2017@yandex.ru>
2022-08-01 17:07:25 +0500

    Solved task 23

---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Программа котрая выдает таблицу кубов от 1 до N");
+
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Таблица квадратов:");
+for (int i=1; i < N; i++) {
+    Console.WriteLine(i*i*i);
+}
