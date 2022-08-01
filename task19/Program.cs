58cff3d89406ce3d0e9af450ca643a6b1213d273
Vlksju <xenya.vol2017@yandex.ru>
2022-08-01 16:52:22 +0500

    Solved task 19

---
+++ task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа котрая определяет является ли число палиндромом.");
+
+Console.WriteLine("Введите пятизначное число");
+string N = Console.ReadLine();
+
+if (N[0] == N[4] && N[1] == N[3]) {
+    Console.WriteLine("Число является палиндромом");
+}
+else {
+    Console.WriteLine("Число не является палиндромом");
+}
+
